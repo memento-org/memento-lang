84f593d8b07dd408308fbf4e631c86b877c8b869
yukikurage <55534323+yukikurage@users.noreply.github.com>
2025-06-29 14:31:28 +0900

    新しいファイルを追加: 基本的な合成型の例を含む compositional_types.mmt

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications  #-}
 
@@ -7,11 +8,16 @@ import qualified Data.Text.IO                               as TIO
 import           Language.Memento.Backend.JS.Codegen        (formatJSProgram)
 import           Language.Memento.Backend.JS.Compile        (compileToJS)
 import           Language.Memento.Data.AST.Tag              (KProgram)
+import           Language.Memento.Data.Environment.Ty       (TyCons)
+import           Language.Memento.Data.Environment.Variance (Variance)
+import           Language.Memento.Data.Ty                   (Ty)
+import           Language.Memento.Data.TypedAST             (TypedAST)
 import           Language.Memento.Parser                    (parseAST)
 import           Language.Memento.TypeSolver                (SolveError (..),
                                                              solveTypedAST)
 import           Language.Memento.TypeSolver.SolveVariances (solveVariancesFromEnv)
-import           Language.Memento.Typing                    (typeProgramWithTyCons)
+import           Language.Memento.Typing                    (TypingError,
+                                                             typeProgramWithTyCons)
 import           System.Directory                           (createDirectoryIfMissing)
 import           System.Environment                         (getArgs)
 import           System.Exit                                (exitFailure,
@@ -24,6 +30,85 @@ import           System.IO                                  (hPrint, hPutStrLn,
 import           System.Process                             (callProcess)
 import           Text.Megaparsec                            (errorBundlePretty,
                                                              parse)
+
+-- | Result of successful compilation pipeline
+data CompilationResult = CompilationResult
+  { crTypedAST :: TypedAST Ty KProgram
+  , crVariances :: TyCons Variance  
+  } deriving (Show)
+
+-- | Errors that can occur during compilation pipeline
+data CompilationError
+  = ParseError String
+  | TypingError TypingError  
+  | SolvingError SolveError
+  deriving (Show)
+
+-- | Core compilation pipeline: parse -> type -> solve
+-- This function extracts the common logic shared by all three commands
+compileMemento :: FilePath -> IO (Either CompilationError CompilationResult)
+compileMemento filePath = do
+  -- Read the file
+  contents <- TIO.readFile filePath
+  
+  -- Parse the file
+  case parse (parseAST @KProgram) filePath contents of
+    Left errorBundle -> 
+      return $ Left $ ParseError $ errorBundlePretty errorBundle
+    Right parsedProgram -> do
+      -- Type check the AST
+      case typeProgramWithTyCons parsedProgram of
+        Left typingError ->
+          return $ Left $ TypingError typingError
+        Right (typedProgramWithConstraints, tyConsWithMaybeVariances) -> do
+          -- Solve variances
+          let solvedVariances = solveVariancesFromEnv tyConsWithMaybeVariances
+          
+          -- Solve type constraints
+          case solveTypedAST solvedVariances typedProgramWithConstraints of
+            Left solveError ->
+              return $ Left $ SolvingError solveError
+            Right fullyTypedProgram ->
+              return $ Right $ CompilationResult fullyTypedProgram solvedVariances
+
+-- | Generate JavaScript code from compilation result and write to file
+-- This function extracts the common logic shared by compileCommand and runCommand
+generateJavaScript :: CompilationResult -> FilePath -> IO FilePath
+generateJavaScript (CompilationResult fullyTypedProgram _) sourceFilePath = do
+  -- Compile to JS IR
+  let jsIR = compileToJS fullyTypedProgram
+  
+  -- Generate JavaScript code  
+  let jsCode = formatJSProgram jsIR
+  let outputFile = "dist" </> replaceExtension (takeFileName sourceFilePath) ".js"
+  
+  -- Create dist directory if it doesn't exist
+  createDirectoryIfMissing True "dist"
+  
+  -- Write to output file
+  TIO.writeFile outputFile jsCode
+  
+  return outputFile
+
+-- | Handle compilation errors with appropriate error messages and exit
+handleCompilationError :: CompilationError -> IO a
+handleCompilationError = \case
+  ParseError err -> do
+    hPutStrLn stderr "Parse error:"
+    hPutStrLn stderr err
+    exitFailure
+  TypingError typingError -> do
+    hPutStrLn stderr "Type error:"
+    hPrint stderr typingError
+    exitFailure
+  SolvingError (ContradictionError err) -> do
+    hPutStrLn stderr "Type constraint solving error:"
+    hPutStrLn stderr err
+    exitFailure
+  SolvingError (UnsolvedVariablesError vars) -> do
+    hPutStrLn stderr "Unsolved type variables:"
+    hPrint stderr vars
+    exitFailure
 
 -- | Main entry point for the Memento compiler
 main :: IO ()
@@ -56,42 +141,14 @@ checkCommand filePath = do
   putStrLn $ "Type checking: " ++ filePath
   putStrLn $ replicate 60 '-'
 
-  -- Read the file
-  contents <- TIO.readFile filePath
-
-  -- Parse the file
-  case parse (parseAST @KProgram) filePath contents of
-    Left errorBundle -> do
-      hPutStrLn stderr "Parse error:"
-      hPutStrLn stderr $ errorBundlePretty errorBundle
-      exitFailure
-    Right ast -> do
+  result <- compileMemento filePath
+  case result of
+    Left err -> handleCompilationError err
+    Right _ -> do
       putStrLn "Parse successful!"
-
-      -- Type check the AST
-      case typeProgramWithTyCons ast of
-        Left typingError -> do
-          hPutStrLn stderr "Type error:"
-          hPrint stderr typingError
-          exitFailure
-        Right (unsolvedTypedAst, tyCons) -> do
-          -- Solve variances
-          let solvedVariances = solveVariancesFromEnv tyCons
-
-          -- Solve type constraints
-          case solveTypedAST solvedVariances unsolvedTypedAst of
-            Left (ContradictionError err) -> do
-              hPutStrLn stderr "Type constraint solving error:"
-              hPutStrLn stderr err
-              exitFailure
-            Left (UnsolvedVariablesError vars) -> do
-              hPutStrLn stderr "Unsolved type variables:"
-              hPrint stderr vars
-              exitFailure
-            Right _typedAST -> do
-              putStrLn $ replicate 60 '-'
-              putStrLn "✓ The file was successfully type checked."
-              exitSuccess
+      putStrLn $ replicate 60 '-'
+      putStrLn "✓ The file was successfully type checked."
+      exitSuccess
 
 -- | Compile a Memento source file to JavaScript
 compileCommand :: FilePath -> IO ()
@@ -99,58 +156,19 @@ compileCommand filePath = do
   putStrLn $ "Compiling: " ++ filePath
   putStrLn $ replicate 60 '-'
 
-  -- Read the file
-  contents <- TIO.readFile filePath
-
-  -- Parse the file
-  case parse (parseAST @KProgram) filePath contents of
-    Left errorBundle -> do
-      hPutStrLn stderr "Parse error:"
-      hPutStrLn stderr $ errorBundlePretty errorBundle
-      exitFailure
-    Right ast -> do
+  result <- compileMemento filePath
+  case result of
+    Left err -> handleCompilationError err
+    Right compilationResult -> do
       putStrLn "Parse successful!"
-
-      -- Type check the AST
-      case typeProgramWithTyCons ast of
-        Left typingError -> do
-          hPutStrLn stderr "Type error:"
-          hPrint stderr typingError
-          exitFailure
-        Right (unsolvedTypedAst, tyCons) -> do
-          -- Solve variances
-          let solvedVariances = solveVariancesFromEnv tyCons
-
-          -- Solve type constraints
-          case solveTypedAST solvedVariances unsolvedTypedAst of
-            Left (ContradictionError err) -> do
-              hPutStrLn stderr "Type constraint solving error:"
-              hPutStrLn stderr err
-              exitFailure
-            Left (UnsolvedVariablesError vars) -> do
-              hPutStrLn stderr "Unsolved type variables:"
-              hPrint stderr vars
-              exitFailure
-            Right solvedTypedAst -> do
-              putStrLn "Type constraint solving successful!"
-
-              -- Compile to JS IR
-              let jsIR = compileToJS solvedTypedAst
-              putStrLn "Compilation to JS IR successful!"
-
-              -- Generate JavaScript code
-              let jsCode = formatJSProgram jsIR
-              let outputFile = "dist" </> replaceExtension (takeFileName filePath) ".js"
-
-              -- Create dist directory if it doesn't exist
-              createDirectoryIfMissing True "dist"
-
-              -- Write to output file
-              TIO.writeFile outputFile jsCode
-
-              putStrLn $ "✓ Successfully compiled to: " ++ outputFile
-              putStrLn ""
-              exitSuccess
+      putStrLn "Type constraint solving successful!"
+      putStrLn "Compilation to JS IR successful!"
+      
+      outputFile <- generateJavaScript compilationResult filePath
+      
+      putStrLn $ "✓ Successfully compiled to: " ++ outputFile
+      putStrLn ""
+      exitSuccess
 
 -- | Compile and run a Memento source file
 runCommand :: FilePath -> IO ()
@@ -158,62 +176,23 @@ runCommand filePath = do
   putStrLn $ "Compiling and running: " ++ filePath
   putStrLn $ replicate 60 '-'
 
-  -- Read the file
-  contents <- TIO.readFile filePath
-
-  -- Parse the file
-  case parse (parseAST @KProgram) filePath contents of
-    Left errorBundle -> do
-      hPutStrLn stderr "Parse error:"
-      hPutStrLn stderr $ errorBundlePretty errorBundle
-      exitFailure
-    Right ast -> do
+  result <- compileMemento filePath
+  case result of
+    Left err -> handleCompilationError err
+    Right compilationResult -> do
       putStrLn "Parse successful!"
+      putStrLn "Type constraint solving successful!"
+      putStrLn "Compilation to JS IR successful!"
+      
+      outputFile <- generateJavaScript compilationResult filePath
+      
+      putStrLn $ "✓ Successfully compiled to: " ++ outputFile
+      putStrLn ""
+      putStrLn "Running JavaScript:"
+      putStrLn $ replicate 60 '-'
 
-      -- Type check the AST
-      case typeProgramWithTyCons ast of
-        Left typingError -> do
-          hPutStrLn stderr "Type error:"
-          hPrint stderr typingError
-          exitFailure
-        Right (unsolvedTypedAst, tyCons) -> do
-          -- Solve variances
-          let solvedVariances = solveVariancesFromEnv tyCons
-
-          -- Solve type constraints
-          case solveTypedAST solvedVariances unsolvedTypedAst of
-            Left (ContradictionError err) -> do
-              hPutStrLn stderr "Type constraint solving error:"
-              hPutStrLn stderr err
-              exitFailure
-            Left (UnsolvedVariablesError vars) -> do
-              hPutStrLn stderr "Unsolved type variables:"
-              hPrint stderr vars
-              exitFailure
-            Right solvedTypedAst -> do
-              putStrLn "Type constraint solving successful!"
-
-              -- Compile to JS IR
-              let jsIR = compileToJS solvedTypedAst
-              putStrLn "Compilation to JS IR successful!"
-
-              -- Generate JavaScript code
-              let jsCode = formatJSProgram jsIR
-              let outputFile = "dist" </> replaceExtension (takeFileName filePath) ".js"
-
-              -- Create dist directory if it doesn't exist
-              createDirectoryIfMissing True "dist"
-
-              -- Write to output file
-              TIO.writeFile outputFile jsCode
-
-              putStrLn $ "✓ Successfully compiled to: " ++ outputFile
-              putStrLn ""
-              putStrLn "Running JavaScript:"
-              putStrLn $ replicate 60 '-'
-
-              -- Run the JavaScript with Node.js
-              callProcess "node" [outputFile]
-              putStrLn ""
-              putStrLn "✓ Execution completed"
-              exitSuccess
+      -- Run the JavaScript with Node.js
+      callProcess "node" [outputFile]
+      putStrLn ""
+      putStrLn "✓ Execution completed"
+      exitSuccess
